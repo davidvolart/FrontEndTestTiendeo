96aefdc2d1e9520c4e4b8353352bada574d9e893
David <dvolart@edu.tecnocampus.cat>
2019-08-31 11:35:47 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Front end tiendeo test
         </a>
       </header>
     </div>
